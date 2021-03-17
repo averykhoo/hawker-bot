320585a2afcece89f417bc6d9bc4d619e5815455
Avery Khoo <averykhoo@gmail.com>
2021-03-17 18:15:55 +0800

    work with location data

--- echo_bot.py
+++ echo_bot.py
@@ -15,12 +15,13 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
+# Define a few command handlers.
+# These usually take the two arguments update and context.
+# Error handlers also receive the raised TelegramError object in error.
 def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
     assert isinstance(update, Update)
     assert isinstance(context, CallbackContext)
-    """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
 
@@ -37,8 +38,32 @@ def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
 
 
+def location(update: Update, context: CallbackContext):
+    if update.message is not None:
+        lines = [
+            f'longitude:              {update.message.location.longitude}',
+            f'latitude:               {update.message.location.latitude}',
+            f'horizontal_accuracy:    {update.message.location.horizontal_accuracy}',
+            f'live_period:            {update.message.location.live_period}',
+            f'heading:                {update.message.location.heading}',
+            f'proximity_alert_radius: {update.message.location.proximity_alert_radius}',
+        ]
+        update.message.reply_text('\n'.join(lines))
+    elif update.edited_message is not None:
+        lines = [
+            f'longitude:              {update.edited_message.location.longitude}',
+            f'latitude:               {update.edited_message.location.latitude}',
+            f'horizontal_accuracy:    {update.edited_message.location.horizontal_accuracy}',
+            f'live_period:            {update.edited_message.location.live_period}',
+            f'heading:                {update.edited_message.location.heading}',
+            f'proximity_alert_radius: {update.edited_message.location.proximity_alert_radius}',
+        ]
+        update.edited_message.reply_text('\n'.join(lines))
+    else:
+        print(update)
+
+
 def ignore(update: Update, context: CallbackContext):
-    """Send a message when the command /start is issued."""
     print(context)
     print(update)
     update.message.reply_text('cannot handle this message type')
@@ -49,7 +74,7 @@ def error(update: Update, context: CallbackContext):
     print(update)
     print(context)
     logger.warning(f'Update "{update}" caused error "{context.error}"')
-    update.message.reply_text('message triggered an error')
+    raise context.error
 
 
 if __name__ == '__main__':
@@ -65,6 +90,7 @@ if __name__ == '__main__':
 
     # handle non-commands
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    updater.dispatcher.add_handler(MessageHandler(Filters.location, location))
     updater.dispatcher.add_handler(MessageHandler(Filters.all, ignore))
 
     # log all errors
@@ -73,7 +99,6 @@ if __name__ == '__main__':
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT.
+    # This should be used most of the time, since start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
