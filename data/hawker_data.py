5e449be91d2aa27a9d8f22f618201b3a3b92e302
Avery Khoo <averykhoo@gmail.com>
2021-03-18 23:49:37 +0800

    figure out hawker data

--- data/hawker_data.py
+++ data/hawker_data.py
@@ -1,11 +1,8 @@
-import datetime
 import json
-from pathlib import Path
 from pprint import pprint
 
 import pandas as pd
 import requests
-from tabulate import tabulate
 
 RESOURCE_IDS = {
     'Dates of Hawker Centres Closure':
@@ -16,28 +13,72 @@ RESOURCE_IDS = {
         '34f86c3e-a90c-4de9-a69f-afc86b7f31b6'
 }
 
-if __name__ == '__main__':
-    for resource_name, resource_id in RESOURCE_IDS.items():
-        r = requests.get('https://data.gov.sg/api/action/datastore_search',
-                         params={
-                             'resource_id': resource_id,
-                             'limit':       1,
-                         })
-        data = json.loads(r.content)
-        pprint(data)
 
+def query_onemap(query):
+    results = []
+    page_num = 0
+    total_pages = 1
+    while page_num < total_pages:
+        page_num += 1
+        r = requests.get('https://developers.onemap.sg/commonapi/search',
+                         params={'searchVal':      query,
+                                 'returnGeom':     'Y',
+                                 'getAddrDetails': 'Y',
+                                 'pageNum':        page_num,
+                                 })
+        data = json.loads(r.content)
+        results.extend(data['results'])
+        total_pages = data['totalNumPages']
+        assert page_num == data['pageNum']
+        if page_num == total_pages:
+            assert len(results) == data['found']
+    return results
+
+
+def convert(lat: float, lon: float):
+    """
+    3414(SVY21) to 4326(WGS84)
+    """
+    r = requests.get('https://developers.onemap.sg/commonapi/convert/3414to4326',
+                     params={'X': lat,
+                             'Y': lon,
+                             })
+    data = json.loads(r.content)
+    return data['latitude'], data['longitude']
+
+
+def get_resource(resource_id: str):
+    limit = 99999  # just for starters
+    r = requests.get('https://data.gov.sg/api/action/datastore_search',
+                     params={
+                         'resource_id': resource_id,
+                         'limit':       limit,
+                     })
+    data = json.loads(r.content)
+    if data['result']['total'] > limit:
+        limit = data['result']['total']
         r = requests.get('https://data.gov.sg/api/action/datastore_search',
                          params={
                              'resource_id': resource_id,
-                             'limit':       data['result']['total'],
+                             'limit':       limit,
                          })
         data = json.loads(r.content)
-        df = pd.DataFrame(data['result']['records'])
-        df = df.sort_values(by='_id')
-        df = df[[field['id'] for field in data['result']['fields']]]
-        print(tabulate(df, headers=df.columns))
-        safe_name = '-'.join(''.join(char if char.isalpha() else ' ' for char in resource_name.lower()).split())
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
-        path = Path(f'{safe_name}/{safe_name}--{timestamp}.csv')
-        path.parent.mkdir(parents=True, exist_ok=True)
-        df.to_csv(path, index=False)
+    df = pd.DataFrame(data['result']['records'])
+    df = df.sort_values(by='_id')
+    df = df[[field['id'] for field in data['result']['fields']]]
+    return df
+
+
+if __name__ == '__main__':
+    pprint(query_onemap('Punggol Town Hub Hawker Centre'))
+    pprint(convert(35955.52, 43336.13))
+
+    # for resource_name, resource_id in RESOURCE_IDS.items():
+    #     df = get_resource(resource_id)
+    #     print(tabulate(df, headers=df.columns))
+    #
+    #     safe_name = '-'.join(''.join(char if char.isalpha() else ' ' for char in resource_name.lower()).split())
+    #     timestamp = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
+    #     path = Path(f'{safe_name}/{safe_name}--{timestamp}.csv')
+    #     path.parent.mkdir(parents=True, exist_ok=True)
+    #     df.to_csv(path, index=False)
---
+++ data/hawkers.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+from typing import Tuple
+
+
+@dataclass
+class Hawker:
+    name: str  # NAME
+    latitude: float  # WGS84 (converted from SVY21 via OneMap API because this is occasionally missing)
+    longitude: float  # WGS84 (converted from SVY21 via OneMap API because this is occasionally missing)
+    # landxaddresspoint: float  # SVY21 geodetic datum
+    # landyaddresspoint: float  # SVY21 geodetic datum
+
+    address_myenv: Optional[str]
+    description_myenv: Optional[str]  # DESCRIPTION_MYENV
+
+    # addresstype: Optional[str]  # 'I' or missing
+    # addressunitnumber: Optional[str]  # missing
+    # addressfloornumber: Optional[str]  # missing
+    addressblockhousenumber: Optional[str]
+    addressstreetname: Optional[str]
+    addressbuildingname: Optional[str]
+    addresspostalcode: Optional[int]  # 6 digits max
+
+    region: Optional[str]  # REGION
+    photourl: Optional[str]  # PHOTOURL
+    no_of_food_stalls: int  # if missing, assume zero available
+    no_of_market_stalls: int  # if missing, assume zero available
+    status: str  # Existing, Existing (new), Proposed, Under Construction
+
+    # rnr_status: Optional[str]  # repairs & redecoration (contains dates!)
+    rnr_period: Optional[Tuple[datetime.date, datetime.date]]
+
+    # est_original_completion_date: Optional[str]  # probably when it was built? parse as '%d/%m/%Y' or '%Y'
+    est_original_completion_year: Optional[int]
+
+    # approximate_gfa: Optional[float]  # gross floor area
+    # awarded_date: str  # ??? parse as '%d/%m/%Y'
+    # cleaningenddate: str  # use other API for this
+    # cleaningstartdate: str  # use other API for this
+    # fmel_upd_d: str  # something update date? parse as '%Y%m%d%H%M%S'
+    # hup_completion_date: str  # related to Hawker Centres Upgrading Programme (HUP)
+    # description: str  # related to Hawker Centres Upgrading Programme (HUP)
+    # hyperlink: str  # missing
+    # implementation_date: str  # ???
+    # inc_crc: str  # 16 hexadecimal chars
+    # info_on_co_locators: str  # nearby amenities? split by '/' to get a list
