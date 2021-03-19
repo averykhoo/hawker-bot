04a0a43ea3350e4b70363b10836b2a6bc97461b9
Avery Khoo <averykhoo@gmail.com>
2021-03-19 18:20:28 +0800

    update script

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ DATE=$(date "+%Y-%m-%d--%H-%M-%S")
 echo "to view stdout, tail ./logs/$DATE.log"
 
 echo "starting..."
-python hawker_bot.py &> "logs/$DATE.log"
-echo "stopped"
+nohup python hawker_bot.py &> "logs/$DATE.log"
+echo $! > nohup_pid.txt
+
+echo "to stop: kill -9 $(cat nohup_pid.txt)"
