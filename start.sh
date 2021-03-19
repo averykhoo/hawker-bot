ecae1a37983a387594e8aa4a5ea33bafefcfb2e4
Avery Khoo <averykhoo@gmail.com>
2021-03-19 18:21:53 +0800

    tweak script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ DATE=$(date "+%Y-%m-%d--%H-%M-%S")
 echo "to view stdout, tail ./logs/$DATE.log"
 
 echo "starting..."
-nohup python hawker_bot.py &> "logs/$DATE.log"
+nohup python hawker_bot.py > "logs/$DATE.log" 2>&1 &
 echo $! > nohup_pid.txt
 
 echo "to stop: kill -9 $(cat nohup_pid.txt)"
