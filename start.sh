0e80793ef6d1da9512afaf182642162499388bc3
Avery Khoo <averykhoo@gmail.com>
2021-03-20 16:49:07 +0800

    fix start sh

--- start.sh
+++ start.sh
@@ -4,11 +4,9 @@ mkdir -p logs
 DATE=$(date "+%Y-%m-%d--%H-%M-%S")
 echo "to view stdout, tail ./logs/$DATE.log"
 
-echo "kill -9 " > stop.sh
+touch stop.sh
 chmod +x stop.sh
 echo "starting..."
 nohup python hawker_bot.py > "logs/$DATE.log" 2>&1 &
-echo $! >> stop.sh
-
+echo echo "kill -9 $!" > stop.sh
 echo "to stop: ./stop.sh"
-echo "to stop: kill -9 $(cat nohup_pid.txt)"
