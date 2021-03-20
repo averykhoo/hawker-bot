4787186edc0aa545c8eb58bdbe5575270424d560
Avery Khoo <averykhoo@gmail.com>
2021-03-20 16:45:47 +0800

    autogenerate stop.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ mkdir -p logs
 DATE=$(date "+%Y-%m-%d--%H-%M-%S")
 echo "to view stdout, tail ./logs/$DATE.log"
 
+echo "kill -9 " > stop.sh
+chmod +x stop.sh
 echo "starting..."
 nohup python hawker_bot.py > "logs/$DATE.log" 2>&1 &
-echo $! > nohup_pid.txt
+echo $! >> stop.sh
 
+echo "to stop: ./stop.sh"
 echo "to stop: kill -9 $(cat nohup_pid.txt)"
