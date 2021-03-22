06a7855eeaf74987602b738795046387f3f5a5d5
Avery Khoo <averykhoo@gmail.com>
2021-03-22 23:54:25 +0800

    add back the code to log stdout and stderr, but comment it out

--- start.sh
+++ start.sh
@@ -7,9 +7,16 @@ if [[ -e "stop.sh" ]]; then
     exit 0
 fi
 
+#TIMESTAMP=$(date "+%Y-%m-%d--%H-%M-%S")
+#LOGDIR="./logs"
+#mkdir -p $LOGDIR
+#LOGFILE="$LOGDIR/hawker-bot--$TIMESTAMP.log"
+#echo "to view stdout, tail $LOGFILE"
+
 touch stop.sh
 chmod +x stop.sh
 echo "starting..."
+#nohup python hawker_bot.py > $LOGFILE 2>&1 &
 nohup python hawker_bot.py > /dev/null 2>&1 &
 echo "kill -9 $!" > stop.sh
 # shellcheck disable=SC2016
