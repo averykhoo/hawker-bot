9b97e1e8777aaddb6d7a7bcac312921fd7c627b5
Avery Khoo <averykhoo@gmail.com>
2021-05-28 14:12:16 +0800

    maybe mimic part of the starlette route api

--- fast_bot.py
+++ fast_bot.py
@@ -17,7 +17,6 @@
 # from telegram.ext import Filters
 # from telegram.ext import InlineQueryHandler
 # from telegram.ext import MessageHandler
-# from telegram.ext import Updater
 #
 # import utils
 # from api_wrappers.location import Location
@@ -643,24 +642,96 @@
 #     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT.
 #     # This should be used most of the time, since start_polling() is non-blocking and will stop the bot gracefully.
 #     updater.idle()
+from typing import Any
+from typing import Callable
+from typing import MutableMapping
+from typing import Tuple
+from typing import TypeVar
 
+from telegram.ext import Updater
+
+# fastapi.types.DecoratedCallable: stricter type inference, guaranteeing same type signature for decorated function
+DecoratedCallable = TypeVar("DecoratedCallable", bound=Callable[..., Any])
+
+# Scope = MutableMapping[str, Any]
+#
+# class BaseRoute:
+#     def matches(self, scope: Scope) -> bool:
+#         raise NotImplementedError()  # pragma: no cover
+#
+#     def partially_matches(self, scope: Scope) -> bool:
+#         raise NotImplementedError()  # pragma: no cover
+#
+#     async def handle(self, scope: Scope, receive: Receive, send: Send) -> None:
+#         raise NotImplementedError()  # pragma: no cover
+#
+#     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+#         """
+#         A route may be used in isolation as a stand-alone ASGI app.
+#         This is a somewhat contrived case, as they'll almost always be used
+#         within a Router, but could be useful for some tooling and minimal apps.
+#         """
+#         match, child_scope = self.matches(scope)
+#         if match == Match.NONE:
+#             if scope["type"] == "http":
+#                 response = PlainTextResponse("Not Found", status_code=404)
+#                 await response(scope, receive, send)
+#             elif scope["type"] == "websocket":
+#                 websocket_close = WebSocketClose()
+#                 await websocket_close(scope, receive, send)
+#             return
+#
+#         scope.update(child_scope)
+#         await self.handle(scope, receive, send)
 
 class FastBot:
     def __init__(self, api_key: str):
-        self.api_key = api_key  # todo: test api key?
+        self.bot_updater = Updater(api_key)
 
-        self.before_middleware = []
+        self.middleware = []  # usually logging?
 
         self.keyword_handlers = dict()
-        self.command_handlers = dict()
-        self.regex_handlers = list()
+        self.command_handlers = dict()  # including start deep links
+        self.regex_handlers = []
 
         self.message_handler = None
         self.inline_handler = None
 
-        self.after_middleware = []
-
         self.error_handler = None
 
-    def start(self):
-        ...
+    def run_forever(self):
+        # Start the Bot
+        self.bot_updater.start_polling()
+
+        # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT.
+        # This should be used most of the time, since start_polling() is non-blocking and will stop the bot gracefully.
+        self.bot_updater.idle()
+
+    def add_command(self, command, endpoint):
+        # check for duplicates
+        if command in self.command_handlers:
+            raise KeyError(command)
+
+        # do something
+        print(command, endpoint.__name__)
+
+    def command(self,
+                command: str,
+                /, *,
+                allow_backslash: bool = True,
+                ) -> Callable[[DecoratedCallable], DecoratedCallable]:
+        # we don't need functools.wraps because we're not actually wrapping the function
+        def decorator(endpoint: DecoratedCallable) -> DecoratedCallable:
+            self.add_command(command, endpoint)
+            return endpoint
+
+        return decorator
+
+
+if __name__ == '__main__':
+    fb = FastBot('asd')
+
+
+    @fb.command('a')
+    def func():
+        return 1
